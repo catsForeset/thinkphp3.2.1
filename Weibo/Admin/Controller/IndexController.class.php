29193ec255772668c60ca6143dbc5cc4ee39932a
luoyan <luoyaninneu@163.com>
2018-05-23 18:19:22 +0800

    添加新模块

---
+++ Weibo/Admin/Controller/IndexController.class.php
@@ -0,0 +1,13 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index()
+    {
+        echo 'Weibo Admin';
+    }
+    public function test(){
+        echo 'Admin ok';
+    }
+}
---
+++ Weibo/Admin/Controller/UserController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018\5\23 0023
+ * Time: 17:24
+ */
+namespace Admin\Controller;
+use Think\Controller;
+class UserController extends Controller
+{
+    public function index(){
+        echo 'Weibo Admin UserController';
+    }
+}
