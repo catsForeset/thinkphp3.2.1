97a591486d358b7a9f362ff8fa18b89a77bacdc1
luoyan <luoyaninneu@163.com>
2018-05-23 17:28:47 +0800

    first push

---
+++ Weibo/Home/Controller/UserController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018\5\23 0023
+ * Time: 17:24
+ */
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller
+{
+    public function index(){
+        echo 'UserController';
+    }
+}
