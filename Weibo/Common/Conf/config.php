076406d91b930e8f91697c2be4208187d070feb9
luoyan <luoyaninneu@163.com>
2018-05-23 18:15:38 +0800

    模块禁用等

--- Weibo/Common/Conf/config.php
+++ Weibo/Common/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'MODULE_DENY_LIST'=>array('Common','Runtime'),//禁止访问模块Common,Runtime,这两个默认不能访问。去掉后允许访问。注释掉恢复默认设置。
+    'MODULE_ALLOW_LIST'=>array('Admin','Weibo'),//允许访问。去掉后拒绝访问。注释掉恢复默认设置。
+//    'DEFAULT_MODULE'=>'Admin',//默认加载模块，进入项目thinkphp3.2.1直接访问此模块
+//    'MULTI_MODULE'=>false,//只允许单模块
 );
